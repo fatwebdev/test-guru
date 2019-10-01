70acb127d81c04c11abcf42c315113b834ac5dbc
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-01 14:39:29 +0300

    add config for timezone and locale

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module TestGuru
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # set time zone
+    config.time_zone = 'UTC'
+
+    # set default locale
+    config.i18n.default_locale = :en
   end
 end
