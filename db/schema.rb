c5facf4a033188cb7a9fee6cc07dbaa426133721
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-02 13:02:49 +0300

    change migration order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_064718) do
+ActiveRecord::Schema.define(version: 2019_10_02_064606) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
