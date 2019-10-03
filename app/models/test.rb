0516a28b19720d85f4f64556422e6a69798ea110
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-03 23:23:18 +0300

    add PassingTest model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  has_many :passing_tests
+  has_many :users, through: :passing_tests
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :passing_tests
+  has_many :tests, through: :passing_tests
 end
