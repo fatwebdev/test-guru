a91bc683dec73b341297b9a627eb4b443683cc86
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-04 08:55:12 +0300

    add Test.sorted_desc_by_category_title

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,4 +3,11 @@ class Test < ApplicationRecord
 
   has_many :passing_tests
   has_many :users, through: :passing_tests
+
+  def self.sorted_desc_by_category_title(title)
+    category = Category.find_by(title: title)
+    return [] if category.nil?
+
+    Test.order(id: :desc).where(category_id: category.id)
+  end
 end
