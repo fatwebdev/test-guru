1e432ce0ec4a47e2878560fc13eb06dd3ce12ff0
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-05 11:53:05 +0300

    add associations between test and question

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: :User
 
+  has_many :questions
   has_many :passing_tests
   has_many :users, through: :passing_tests
 
