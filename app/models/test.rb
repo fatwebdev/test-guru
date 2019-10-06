03eb62c6ac6bda6b92ac746e96fe9034fcb9c563
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-06 16:17:33 +0300

    add Test scopes by level

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,11 @@ class Test < ApplicationRecord
   has_many :passing_tests
   has_many :users, through: :passing_tests
 
+  scope :level, ->(level) { where(level: level) }
+  scope :level_easy, -> { level(0..1) }
+  scope :level_medium, -> { level(2..4) }
+  scope :level_hard, -> { level(5..Float::INFINITY) }
+
   def self.sorted_desc_by_category_title(title)
     Test
       .joins(:category)
