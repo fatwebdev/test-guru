326a4a5b815a64bc3eab9038dd8f8d4c6ce498ac
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-06 17:06:30 +0300

    add validates for Answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,14 @@
 class Answer < ApplicationRecord
+  validates :body, presence: true
+  validate :validate_max_answers, on: :create
+
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_max_answers
+    errors.add(:base, 'Question can have max 3 answers') if question.answers.count > 3
+  end
 end
