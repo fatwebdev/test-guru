4ed7ff581a8d144e5324f6e7782dd4f564595679
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-06 17:07:33 +0300

    add validates for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :login, :password, presence: true
+
   has_many :created_tests, class_name: :Test, foreign_key: :author_id
   has_many :passing_tests
   has_many :tests, through: :passing_tests
