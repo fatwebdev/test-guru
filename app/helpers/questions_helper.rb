f90d5bafb881dd9c1ac96a3b2f0e85999919899a
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-08 16:35:05 +0300

    add question view helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  QUESTION_HEADERS = {
+    new: ->(title) { "Create New #{title} Question" },
+    edit: ->(title) { "Edit #{title} Question" }
+  }
+
+  def question_header
+    QUESTION_HEADERS[action_name.to_sym].call(@question.test.title)
+  end
 end
