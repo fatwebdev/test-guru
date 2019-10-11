176251648a587ca07ef0bb67c974b7b796ceab5a
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-11 09:47:40 +0300

    refactor move set next question to model callback

--- app/models/passing_test.rb
+++ app/models/passing_test.rb
@@ -4,13 +4,13 @@ class PassingTest < ApplicationRecord
   belongs_to :current_question, class_name: :Question, optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -26,6 +26,10 @@ class PassingTest < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
