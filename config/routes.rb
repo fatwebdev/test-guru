374efa645542664068e8a848f9bee0a560116977
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-12 20:25:08 +0300

    add users/new path for create User

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tests#index'
 
+  resources :users, only: %i[new create]
+
   resources :tests do
     resources :questions, shallow: true, except: :index do
       resources :answers, shallow: true, except: :index
