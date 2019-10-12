8d407b118de130afcd83b0997796bdf0d17f37cf
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-12 19:16:28 +0300

    add flash helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tests_path
     else
+      flash.now[:alert] = 'Incorrect username or password'
       render :new
     end
   end
