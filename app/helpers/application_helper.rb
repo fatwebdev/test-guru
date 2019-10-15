201acd65468de75da3055f8918029bd4055f4818
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-15 19:44:57 +0300

    add able show different flashes from flash_message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
-  def flash_message(message)
-    content_tag :p, flash[:alert], class: 'flash alert' if flash[message]
+  def flash_message
+    flash.map do |type, msg|
+      content_tag :p, msg, class: "flash #{type}"
+    end.join.html_safe
   end
 end
