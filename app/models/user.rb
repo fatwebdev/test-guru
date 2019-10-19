2d096387fc57545fbd1cea17c2d3868ab30df374
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-19 09:29:19 +0300

    add Gist model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
   has_many :created_tests, class_name: :Test, foreign_key: :author_id
   has_many :passing_tests
   has_many :tests, through: :passing_tests
+  has_many :gists
 
   def passing_test(test)
     passing_tests.order(id: :desc).find_by(test_id: test.id)
