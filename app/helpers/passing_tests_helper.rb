cfe42262fee53db8b8cd03c953473b60b452a649
Yan Shevtsov <fatwebdev@gmail.com>
2019-10-21 08:28:52 +0300

    add progressbar bar of passing test

--- app/helpers/passing_tests_helper.rb
+++ app/helpers/passing_tests_helper.rb
@@ -2,4 +2,22 @@ module PassingTestsHelper
   def current_question_number
     @passing_test.test.question_sequence_number(@passing_test.current_question)
   end
+
+  def progress_bar
+    content_tag :div, class: 'progress' do
+      content_tag :div, nil,
+        class: 'progress-bar',
+        role: 'progressbar',
+        style: "width: #{percent_of_passing_test}%",
+        aria_valuenow: percent_of_passing_test,
+        aria_valuemin: 0,
+        aria_valuemax: 100
+    end
+  end
+
+  private
+
+  def percent_of_passing_test
+    current_question_number.to_f / @passing_test.test.questions.count * 100
+  end
 end
